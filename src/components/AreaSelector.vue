8e85711a0ca92608c6b8d4f9934c8d78514eecae
poe1599 <74940748+poe1599@users.noreply.github.com>
2023-01-04 22:53:08 +0800

    Update AreaSelector.vue
    多的

--- src/components/AreaSelector.vue
+++ src/components/AreaSelector.vue
@@ -30,34 +30,7 @@ export default {
 <script setup>
 const visible=true;</script>
 <style lang="scss" scoped>
-.pagination {
-  margin: 16px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 12px;
 
-  span {
-    font-style: normal;   
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 24px;
-    letter-spacing: 0.5px;
-    color: $primary-1;
-  }
-}
-
-.pagination__button--hollow {
-  @include button($primary-1, $white, $white, $bg-1, $primary-1, $primary-1);
-
-  width:44px;
-}
-
-.pagination__button--solid {
-  @include button($white, $text-1, $text-1, $primary-1, $bg-1, $bg-1);
-  
-  width:44px;
-}
 
 .searchInput {
   display: flex;
